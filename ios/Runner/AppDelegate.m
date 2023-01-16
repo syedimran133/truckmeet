eec818a4c37fa33b9c464d88937651d2b24fe4f9
monish152 <monish125@gmail.com>
2023-01-16 22:17:40 +0530

    iOS setting
    iOS setting

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -5,6 +5,7 @@
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    [GMSServices provideAPIKey:@"AIzaSyAwnbQWbsFwMekiP38kM_cujWaae2bQ1UU"];
   [GeneratedPluginRegistrant registerWithRegistry:self];
   // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
